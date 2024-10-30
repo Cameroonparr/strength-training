99203d16d4182461bfae08bc53da1f6a2e6ce54f
Cameroonparr <106214642+Cameroonparr@users.noreply.github.com>
2024-10-30 14:35:27 +1300

    Add files via upload
    initial commit without media

---
+++ data.js
@@ -0,0 +1,7 @@
+//Normally data is stored in DB, when app sends a request to DB, it return this array of data
+//Link: https://www.imdb.com/chart/top/
+let items =[{id: 1, title: "benchpress", image_url: "media/benchpress-slide.jpg",}, 
+    {id: 2, title: "squat", image_url: "media/squat-slide.jpg"},
+    {id: 3, title: "deadlift", image_url: "media/deadlift-slide.jpg"} 
+];
+
